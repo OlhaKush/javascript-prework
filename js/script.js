abcad5a387e7b7fe76f39a2a910592db8f86dd89
Olha Kushyk <olha.kushyk@gmail.com>
2023-01-11 20:15:39 +0200

    Add logic to script

--- js/script.js
+++ js/script.js
@@ -29,3 +29,25 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+} else if ( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+} else if ( computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+} else if ( computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Wygrał komputer!');
+} else if ( computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Wygrał komputer!');
+} else if ( computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('Wygrał komputer!');
+} else if ( computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Remis!');
+} else if ( computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Remis!');
+} else if ( computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('Remis!');
+} else {
+    printMessage('Wybierz swój ruch!');
+}
